d7311d21b1ecff0b8b22dddf2b2f8d232929c75a
Victor <iaw47865910@h13.informatica.escoladeltreball.org>
2017-04-06 21:15:42 +0200

    Uploading .env

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,11 +1,11 @@
-module.exports = function(app){
+module.exports = function (app) {
 
     link = function (req, res) {
         res.render('link', {title: 'Link'});
     };
 
     app.get('/', function (req, res) {
-        res.render('main',{title: 'Solidoy - Main page'});
+        res.render('main', {title: 'Solidoy - Main page'});
     });
 
     app.get('/link', link);
